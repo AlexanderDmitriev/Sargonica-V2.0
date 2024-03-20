764f72bcba9260fbff3f71e1d661da64fff72952
AlexanderDmitriev <alex9110077@gmail.com>
2024-03-21 00:45:36 +0100

    1

---
+++ src/app/App.tsx
@@ -0,0 +1,23 @@
+import Button from '../shared';
+
+const App = () => {
+  return (
+    <>
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#010101',
+        }}
+      >
+        React homework template
+      </div>
+      <Button/>
+    </>
+  );
+};
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './app/App';
+import './index.css';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+
+if (!root) {
+  throw new Error("The element #portal wasn't found");
+}
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
